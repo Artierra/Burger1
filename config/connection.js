4614c651b956ad0d03f690a43366ed156f50aa60
Monica Bongue <muddyforkfarm@gmail.com>
2019-12-17 17:55:10 -0500

    conncection to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
-connection = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "rootroot",
-    database: "burger_db",
-    port: 3306
-});
-
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "rootroot",
+        database: "burger_db",
+        port: 3306
+    });
+}
 connection.connect(function (err) {
     if (err) {
         console.error("there was an error connecting" + err.stack);
@@ -15,4 +19,4 @@ connection.connect(function (err) {
     console.log("connected to my Sql as id " + connection.threadId);
 });
 
-module.exports = connection;
+module.exports = connection
